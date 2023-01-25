97cea4e643c272d536784fe5200896c9d0b49077
billofright <bill.yuan27@gmail.com>
2023-01-24 18:31:18 -0800

    real time graphing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+import io from "socket.io-client";
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+
+import { useEffect, useState } from 'react';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    socket.on("data", (data) => {
+      setMessage(currentData => [...currentData, data]);
+    });
+  }, [socket]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <body>
-          Data:
-        </body>
-      </header>
+      <h1>Current temperature</h1>
+      <LineChart width={500} height={300} data={message}>
+        <XAxis type='number' dataKey='time' allowDecimals='false' domain={['dataMax - 10', 'auto']}/>
+        <YAxis/>
+        <Line type='monotone' dataKey='value' stroke="#8884d8"/>
+      </LineChart>
     </div>
   );
 }
